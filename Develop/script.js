47be851a4b493b33b766738368cbed227a752ab2
aafaydi <97416091+aafaydi@users.noreply.github.com>
2022-02-19 19:21:09 -0500

    updates

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,51 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+var lower = "abcdefghijklmnopqrstuvwxyz";
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var number= "0123456789";
+var symbol= "!@#$%^&*()_+~`|}{[]\:;?><,./-=";
+var allchar= "";
+
+function generatePassword() {
+  allchar= allchar+ lower;
+  
+var passwordlength = prompt("how many charcters is your password would be");
+if( passwordlength < 8){
+  alert("password has to be greater than 8 and less than 128")
+  var passwordlength = prompt("how many charcters is your password would be");
+  }
+  else if(passwordlength>128){
+    alert("password has to be greater than 8 and less than 128")
+  var passwordlength = prompt("how many charcters is your password would be");
+  }
+
+ 
+
+    var upperconf = window.confirm("Would like to add uppercase to you password y/n");
+    if(upperconf){
+      allchar= allchar + upper; 
+    }
+
+    var numberconf =  window.confirm("Would like to add numbers to you password y/n"); 
+    if( numberconf){
+      allchar= allchar + number; 
+    }
+
+    var symbolconf = window.confirm("Would like to add symbol to you password y/n"); 
+    if( symbolconf){
+      allchar= allchar + symbol; 
+    }
+    console.log(allchar);
+
+ var Gpassword="";
+ for( var i=0; i<passwordlength; i++) {
+  Gpassword += allchar.charAt(Math.floor(Math.random() * allchar.length));
+ }
+
+return Gpassword;
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -15,3 +58,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
